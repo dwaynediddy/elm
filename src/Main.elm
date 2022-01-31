954e3d6305ca7b5ae96d957ccd8c9750aae19fd1
dwaynediddy <burrows.dwayne@icloud.com>
2022-01-31 23:27:12 +0100

    multi field input with basic functionn

--- src/Main.elm
+++ src/Main.elm
@@ -8,25 +8,50 @@ main =
     Browser.sandbox {init = init, update = update, view = view}
 
 type alias Model = 
-    { content : String }
+    { name : String
+    , password : String
+    , confirmPassword : String }
+
 
 init : Model 
 init = 
-    { content = "" }
+    Model "" "" ""
 
-type Msg = 
-    Change String
+    -- update
+
+type Msg =
+    Name String
+    | Password String
+    | ConfirmPassword String
 
 update : Msg -> Model -> Model 
 update msg model = 
     case msg of 
-        Change newContent ->
-            { model | content = newContent }
+        Name name ->
+            { model | name = name}
+        Password password ->
+            { model| password = password }
+        ConfirmPassword confirmPassword ->
+            { model | confirmPassword = confirmPassword }
+
+            -- view
 
 view : Model -> Html Msg 
 view model = 
     div []
-    [ input [placeholder "Add Text here", value model.content, onInput Change][],
-    div [] [ text (String.reverse model.content)]
+    [ viewInput "text" "Name" model.name Name
+    , viewInput "text" "Password" model.password Password
+    , viewInput "text" "re-enter Password" model.confirmPassword ConfirmPassword
+    , viewValidation model
     ]
 
+viewInput : String -> String -> String -> (String -> Msg) -> Html Msg
+viewInput t p v toMsg =
+    input [type_ t, placeholder p, value v, onInput toMsg] []
+
+viewValidation: Model -> Html msg
+viewValidation model = 
+    if model.password == model.confirmPassword then
+        div [style "color" "green"] [text "ok"]
+            else
+        div [ style "color" "red"] [text "bad"]
